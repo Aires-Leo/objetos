f171f20649ff78bcdf6a4bd3cf178ccbeaa64114
Aires-Leo <leonardo_aires@live.com>
2023-08-30 21:56:36 -0300

    Aula de objetos

---
+++ index.js
@@ -0,0 +1,43 @@
+//Exercício dos slides
+//1)
+/*
+const filme = {
+    direcao: "James Camaron",
+    nome: "Avatar",
+    lancamento: 2009,
+    atores: ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver", "Stephan Lang"],
+    assistido: "true"
+}
+console.log(filme.direcao)
+console.log(filme.nome)
+console.log(filme["lancamento"])
+console.log(filme["atores"][2])
+console.log(filme["assistido"])
+console.log(filme.atores[1])
+*/
+//2)
+/*
+const pessoa = {
+    nome: "Leonardo",
+    idade: 32,
+    generoMusical: "Rock e Reggae",
+}
+console.log(`O nome da pessoa é ${pessoa.nome}, ela tem ${pessoa.idade} anos e gosta muito de ${pessoa.generoMusical}.`)
+*/
+//3) 
+/*
+const filme = [
+    {ator: "Sam Worthington", personagem: "Jake Sully"},
+    {ator: "Zoe Saldana", personagem: "Neteyam"},
+    {ator: "Sigourney Weaver", personagem: "Kiri"},
+    {ator: "Stephan Lang", personagem: "Coronel Miles"}
+
+]
+console.log(filme[0].ator, '-', filme[0].personagem)
+console.log(filme[1].ator, '-', filme[1].personagem)
+console.log(filme[2].ator, '-', filme[2].personagem)
+console.log(filme[3].ator, '-', filme[3].personagem)
+
+ filme[1].ator= "Xuxa"
+ console.log(filme[1].ator, "-" , filme[1].personagem)
+ */
